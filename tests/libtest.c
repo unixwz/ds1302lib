66820bcb2f2b2f41e94c53390da0b461321272a7
unixwz <unixwz@gmail.com>
2016-09-15 08:33:54 +0300

    tests for testing of read data function

--- tests/libtest.c
+++ tests/libtest.c
@@ -2,7 +2,10 @@
 
 int main()
 {
-	ds1302_set_time(8, 'h', 0, 0);
-	ds1302_set_date(8, 'd');
+	ds1302time_t ds1302time;
+	ds1302_set_time(0x09, 'h', 1, 0);
+	// ds1302_set_date(8, 'd');
+	_delay_us(5);
+	ds1302_get_time(&ds1302time);
 	return 0;
 }
